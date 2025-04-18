d58698d59b2f91731afa98d92d7604f460ea0aa9
ASGT-Research <avsinghrathotft@gmail.gom>
2025-04-18 16:03:23 +0530

    BainarySearchNewAdd

--- DSA/arrayLS.c
+++ DSA/arrayLS.c
@@ -20,16 +20,34 @@ int LargestNumber(int number[]){
   }
   return largest;}
 
+int BinarySearch(int number[],int key){
+  int start=0;
+  int end = MAX-1;
+  while(start <= end){
+    int mid = (start+end)/2;
+    if (number[mid] == key) {
+      return mid;
+    }
+    if (number[mid] < key) {
+      start = mid+1;
+    }else {end =mid-1;
+    }
+  }
+  return -1;
+}
+
 int main()
 {int number[MAX]={2,4,6,8,10,12,14,16,18,20};
   int key ;
   scanf("%d",&key);
   int index = LinearSearch(number,key);
   if (index == -1) {
-    printf("Key Not found ");
-  }else {printf("key is index :%d",index);
+    printf("\n Key Not found ");
+  }else {printf("\n key is index :%d",index);
   }
 
   printf("\n largest Number is :%d\n",LargestNumber(number));
+
+  printf(" BinarySearch techniqe find index is : %d\n",BinarySearch( number, key));
   return 0;
 }
