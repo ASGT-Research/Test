c97020ecb6977668ab00f80fac800f293cd33fc8
ASGT-Research <avsinghrathotft@gmail.gom>
2025-04-18 18:49:56 +0530

    changes

--- DSA/arrayLS.c
+++ DSA/arrayLS.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define MAX 10
 
 int LinearSearch(int number[],int key){
@@ -13,7 +15,7 @@ int LinearSearch(int number[],int key){
 
 int LargestNumber(int number[]){
   int largest = 0;
-  for (int i=0;i<= MAX-1;i++) {
+  for (int i=0;i<= MAX;i++) {
     if (largest < number[i]) {
       largest = number[i];
     }
@@ -37,17 +39,36 @@ int BinarySearch(int number[],int key){
 }
 
 int main()
-{int number[MAX]={2,4,6,8,10,12,14,16,18,20};
-  int key ;
+{   int number[MAX];
+    int key ;
+   printf("\n Enter array element,Element should be integer");
+  for (int i=0;i<MAX;i++) {
+    scanf("%d",&number[i]);
+   }
+  printf("\n Enter Key Value you want to findin array");
   scanf("%d",&key);
-  int index = LinearSearch(number,key);
-  if (index == -1) {
-    printf("\n Key Not found ");
-  }else {printf("\n key is index :%d",index);
+ while(1){
+    int choise;
+
+  printf("\n Enter your choise.\n 1 LinearSearch method \n 2 LargestNumber \n 3 BinarySearch\n press any key for exit");
+  scanf("%d",&choise);
+ 
+
+  switch (choise) {
+    case 1:int index = LinearSearch(number,key);
+    if (index == -1) {printf("\n Key Not found ");}
+    else {printf("\n key is index :%d",index);}
+    break;
+    case 2:
+      printf("\n largest Number is : %d\n",LargestNumber(number));
+    break;
+    case 3:
+    printf(" BinarySearch techniqe find index is : %d\n",BinarySearch( number, key));
+    break;
+  
+    default:
+      printf("%\n Invaild Choise");
+      exit(0);
+    }
   }
-
-  printf("\n largest Number is :%d\n",LargestNumber(number));
-
-  printf(" BinarySearch techniqe find index is : %d\n",BinarySearch( number, key));
-  return 0;
-}
+  return 0;}
